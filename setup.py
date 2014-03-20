2f4467230df0fa3283625cd6f6399aec0e04ac13
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-20 23:56:21 +0000

    setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='identify_wiki',
+    version='0.0.1',
+    author='Tristan Chong',
+    author_email='tristanchong@gmail.com',
+    description='A library for identifying subjects most likely to represent the content of a wiki',
+    license='Other',
+    packages=['identify_wiki'],
+    install_requires=['nlp_services>=0.0.1', 'requests', 'nltk', 'beautifulsoup4'],
+    dependency_links=[ 'https://github.com/relwell/nlp_services/archive/master.zip#egg=nlp_services-0.0.1']
+    )
