c67d12e48afbf087a4592b3a23cbd7371aa0c3aa
Tristan Euan Chong <tristaneuan@gmail.com>
2014-03-21 17:27:09 +0000

    trying with requirements.txt

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     description='A library for identifying subjects most likely to represent the content of a wiki',
     license='Other',
     packages=['identify_wiki'],
-    install_requires=['nlp_services', 'requests', 'nltk', 'beautifulsoup4'],
+    install_requires=['nlp_services>=0.0.1', 'requests', 'nltk', 'beautifulsoup4'],
     dependency_links=['https://github.com/tristaneuan/nlp_services/archive/master.zip#egg=nlp_services-0.0.1']
     )
